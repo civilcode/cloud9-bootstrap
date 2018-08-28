86a216cadb1f39fab9ca072e7e99939f151854c5
Nicholas Henry <nicholas.henry@gmail.com>
2018-08-28 13:16:31 -0400

    feat: add direnv and re-organize install script

--- install.sh
+++ install.sh
@@ -1,33 +1,45 @@
 #!/bin/sh
 
-# docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+# setup `Development` directory
+ mkdir -p ~/Development
 
 # dotfiles
-mkdir -p ~/Development \
-    && cd ~/Development \
-    && git clone https://github.com/civilcode/dotfiles.git \
-    && env RCRC=$HOME/Development/dotfiles/rcrc rcup \
-    && lsrc
+cd ~/Development && \
+  git clone https://github.com/civilcode/dotfiles.git
 
 # rcm
-curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.3.3.tar.gz \
-  && tar -xvf rcm-1.3.3.tar.gz \
-  && cd rcm-1.3.3 \
-  && ./configure \
-  && make \
-  && sudo make install
+cd ~/Development && \
+  curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.3.3.tar.gz && \
+    tar -xvf rcm-1.3.3.tar.gz && \
+    cd rcm-1.3.3 && \
+    ./configure && \
+    make && \
+    sudo make install
+
+# docker-compose
+cd ~/Development && \
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose --version
 
 # hub
-wget https://github.com/github/hub/releases/download/v2.5.0/hub-linux-386-2.5.0.tgz \
-  && tar zvxvf hub-linux-386-2.5.0.tgz \
-  && sudo ./hub-linux-386-2.5.0/install \
-  && rm -rf ./hub-linux-386-2.5.0 \
-  && rm -rf ./hub-linux-386-2.5.0.tgz
+cd ~/Development && \
+  wget https://github.com/github/hub/releases/download/v2.5.0/hub-linux-386-2.5.0.tgz && \
+    tar zvxvf hub-linux-386-2.5.0.tgz && \
+    sudo ./hub-linux-386-2.5.0/install && \
+    rm -rf ./hub-linux-386-2.5.0 && \
+    rm -rf ./hub-linux-386-2.5.0.tgz
+
+# direnv
+cd ~/Development && \
+  git clone http://github.com/direnv/direnv.git && \
+   cd direnv && \
+   make && \
+   sudo make install
 
 # setup
-echo "source $HOME/.civilcode.zshrc" >> ~/.bashrc
-env RCRC=$HOME/Development/dotfiles/rcrc rcup -f
-source ~/.bashrc
+env RCRC=$HOME/Development/dotfiles/rcrc rcup -f && lsrc
+echo "source $HOME/.civilcode.shrc" >> ~/.bashrc
+echo 'eval "$(direnv hook bash)"' >> ~/.shrc.local
+
+echo "Finished. Run `source ~/.bashrc`."
